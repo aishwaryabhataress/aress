4a95dae328b1bedbdc1e6a0cf10ba480f56b7d4f
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-05 17:11:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -403,7 +403,9 @@ html {
 
 </style>
 
-<script type="application/javascript">
+<script type="application/javascript"  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous">
 
 
    $(".input input").focus(function() {
