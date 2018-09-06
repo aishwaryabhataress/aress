08898ec7f195f2637433bc7ecb17a684bffe7128
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-06 11:37:53 +0530

    Update validate.php

--- validate.php
+++ validate.php
@@ -3,7 +3,7 @@
 // Grab User submitted information
 
 // Connect to the database
-$db = pg_connect("host=ec2-54-227-244-12.compute-1.amazonaws.com port=5432 dbname=d544g2lkvj2uqn user=jwvfktfyuyftwy password=cd2fd6c6fe21c91ec21198788e2eb438b39d7b609127f5dde02af95849278ecc");
+$db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
 if (!$db) 
 {
 	echo "An error occurred.\n";
@@ -33,3 +33,4 @@ if($flag == 0)
 	//header("Location:index.php");
 }
 ?>
+
