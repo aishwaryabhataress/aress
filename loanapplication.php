05df47fb793a4c8f75b8617a82c52e4172d2bfa0
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-06 15:23:33 +0530

    Update loanapplication.php

--- loanapplication.php
+++ loanapplication.php
@@ -1,6 +1,17 @@
 <html>
 <title>Loan Application Page </title>
-<head> </head>
+<head>
+  <style> 
+input[type=text] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 2px solid red;
+}
+</style>
+  </head>
 <body>
   <h1>Loan Application Form</h1>
   <h3>Please fill in all needed information in the loan application form below to request a loan from your organization. Information regarding income assets are requested for qualification. If you have questions on how to fill this loan application form, feel free to contact the Loan Officer or any other member of the Loan Department.</h3>
