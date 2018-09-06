0fd9b831be5bb9d3d14c7a4129c2227c50cdc099
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-06 16:03:49 +0530

    Update loanapplication.php

--- loanapplication.php
+++ loanapplication.php
@@ -11,6 +11,7 @@
 <body>
   <h1>Loan Application Form</h1>
   <h3>Please fill in all needed information in the loan application form below to request a loan from your organization. Information regarding income assets are requested for qualification. If you have questions on how to fill this loan application form, feel free to contact the Loan Officer or any other member of the Loan Department.</h3>
+  <hr style="border: 4px solid blue;" />
   <form name="loanapplication">
     Personal Information
     Please complete the form below to submit your loan application online. Your application is secure and we do not share any of your information.
