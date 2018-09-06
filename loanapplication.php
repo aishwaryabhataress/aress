8c4aed4a28d5c0857049a66055a38276972a4664
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-06 16:28:55 +0530

    Update loanapplication.php

--- loanapplication.php
+++ loanapplication.php
@@ -2,15 +2,18 @@
 <title>Loan Application Page </title>
 <head>
   <style> 
-    .h4{
-       align: justify;
-
-    }
+    p {
+    display: block;
+    -webkit-margin-before: 1em;
+    -webkit-margin-after: 1em;
+    -webkit-margin-start: 0px;
+    -webkit-margin-end: 0px;
+}
 </style>
   </head>
 <body>
   <h1>Loan Application Form</h1>
-  <h4>Please fill in all needed information in the loan application form below to request a loan from your organization. Information regarding income assets are requested for qualification. If you have questions on how to fill this loan application form, feel free to contact the Loan Officer or any other member of the Loan Department.</h4>
+  <p>Please fill in all needed information in the loan application form below to request a loan from your organization. Information regarding income assets are requested for qualification. If you have questions on how to fill this loan application form, feel free to contact the Loan Officer or any other member of the Loan Department.</p>
 <hr style="border: 3px solid#04C5F9;" />
   <form name="loanapplication">
     Personal Information
