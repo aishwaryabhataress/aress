fb9388bd4bba2631fb8929e7a36f884a39c20e25
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-06 11:59:00 +0530

    Update validsignup.php

--- validsignup.php
+++ validsignup.php
@@ -1,11 +1,11 @@
 <?php
-$db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
+$db = pg_connect("host=ec2-54-227-244-12.compute-1.amazonaws.com port=5432 dbname=d544g2lkvj2uqn user=jwvfktfyuyftwy password=cd2fd6c6fe21c91ec21198788e2eb438b39d7b609127f5dde02af95849278ecc");
 if (!$db) 
 {
 	echo "An error occurred.\n";
 	exit;	
 }
-$query = "INSERT INTO salesforce.contact(FirstName, LastName,Phone, MobilePhone, Email, Password__c,User_Key__c) VALUES('$_POST[firstname]','$_POST[lastname]','$_POST[phonenumber]', '$_POST[mobilenumber]','$_POST[emailid]','$_POST[password]','$_POST[userkey]');";
+$query = "INSERT INTO salesforce.contact(FirstName, LastName, MobilePhone, Email, Password__c,User_Key__c) VALUES('$_POST[firstname]','$_POST[lastname]','$_POST[mobilenumber]','$_POST[emailid]','$_POST[password]','$_POST[userkey]');";
 $result= pg_query($query);
 $flag = 0;
 $query1 = "SELECT Email ,Password__c FROM salesforce.contact;";
