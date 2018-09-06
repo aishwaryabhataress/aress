2715285f4008859169715f05fb34cd9e26bdd83d
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-06 13:18:58 +0530

    Create loanapplication.php

---
+++ loanapplication.php
@@ -0,0 +1,13 @@
+<html>
+<title>Loan Application Page </title>
+<head> </head>
+<body>
+  <h1>Loan Application Form</h1>
+  <h3>Please fill in all needed information in the loan application form below to request a loan from your organization. Information regarding income assets are requested for qualification. If you have questions on how to fill this loan application form, feel free to contact the Loan Officer or any other member of the Loan Department.</h3>
+  <form name="loanapplication">
+    Personal Information
+    
+  </form>
+
+</body>
+</html>
