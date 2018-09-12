640a42deba487a689d97013fd43dea25b406bcfa
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-12 10:22:40 +0530

    Update loanapplication.php

--- loanapplication.php
+++ loanapplication.php
@@ -69,12 +69,7 @@
 	  </div>
   </form>
 	  
-	  <?php
-		if(isset($_POST['submit'])){
-		$selected_val = $_POST['loantype']; // Storing Selected Value In Variable
-		echo "You have selected :" .$selected_val; // Displaying Selected Value
-		}
-	?>
+	 
   </div>
 </body>
 </html>
