6897d4c29f2475bab7ea68152f53055990a80699
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-12 10:51:34 +0530

    Update loanapplication.php

--- loanapplication.php
+++ loanapplication.php
@@ -64,8 +64,8 @@
       
 	  <br><br>
 	  <div>
-	  <input type="submit" name="submit" value="Submit" style="margin-top: -100px;margin-left: 350px; text-align:center; background-color:#04C5F9; color:white;"/>
-	  <a href="home.php"><input type="button" name="back" value="Back" style="margin-top-100px;margin-left: 450px; text-align:center; background-color:#04C5F9; color:white;"/>
+	  <input type="submit" name="submit" value="Submit" style="margin-top: -50px;margin-left: 350px; text-align:center; background-color:#04C5F9; color:white;"/>
+	  <a href="home.php"><input type="button" name="back" value="Back" style="margin-top-140px;margin-left: 450px; text-align:center; background-color:#04C5F9; color:white;"/>
 	  </div>
   </form>
 	  
