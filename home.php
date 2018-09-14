f0a7008d2a870176e7fb0ede12c67f367132190f
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-14 14:07:56 +0530

    Update home.php

--- home.php
+++ home.php
@@ -204,39 +204,39 @@ li a.active {
 <div class="mySlides fade">
   <div class="numbertext">1 / 6</div>
   <img src="http://www.jucbwai.com/images/Vehicle-loan.jpg" style="width:100%">
-	<button class = "bttn">Car loan</button>
-	<button class = "bttn">Bike loan</button>
-  <div class="text">Caption Text</div>
+	<div style="display: inline-block; width:50%;">	<button class = "bttn">Car loan</button>  </div>
+	<div style="display: inline-block; width:50%;"> <button class = "bttn">Bike loan</button> </div>
+  
 </div>
 
 <div class="mySlides fade">
   <div class="numbertext">2 / 6</div>
   <img src="https://www.sbiloanrates.com/wp-content/uploads/2017/03/home-loan-emi-calculator-india.jpg" style="width:100%">
-  <div class="text">Caption Two</div>
+ 
 </div>
 
 <div class="mySlides fade">
   <div class="numbertext">3 / 6</div>
   <img src="http://www.dealsofloan.com/wp-content/uploads/2016/08/Marriage-Loans-Market-in-India1.jpg" style="width:100%;">
-  <div class="text">Caption Three</div>
+ 
 </div>
 		
 <div class="mySlides fade">
   <div class="numbertext">4 / 6</div>
   <img src="https://s3-ap-southeast-1.amazonaws.com/buddy4study/blog/uploads/2018/05/07163939/How-to-manage-your-education-loan_Buddy4Study.jpg" style="width:100%;">
-  <div class="text">Caption Three</div>
+ 
 </div>
 	
 <div class="mySlides fade">
   <div class="numbertext">5 / 6</div>
   <img src="http://www.waranabank.com/uploads/waranabank/banner6.png" style="width:100%;">
-  <div class="text">Caption Three</div>
+ 
 </div>
 		
 <div class="mySlides fade">
   <div class="numbertext">6 / 6</div>
   <img src="https://qph.fs.quoracdn.net/main-qimg-36323580662716ccc05eb4b8355899d5" style="width:100%;">
-  <div class="text">Caption Three</div>
+  
 </div>
 
 <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
