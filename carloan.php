e92551b79a70369dbf4a548868b21b4fde109d8d
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-14 18:22:03 +0530

    Update carloan.php

--- carloan.php
+++ carloan.php
@@ -36,7 +36,7 @@ label {
     $loanprincipal=$tenure="";
   ?>
 <body>
-    <div class="container">
+   <!-- <div class="container">
   <form action="loanapplication.php" method="post">
     <div style="display:inline-block;width:100%;">
       		<div style="width:25%;float:left;text-align:right;"> 
@@ -59,6 +59,24 @@ label {
 	<div style="display:inline;color:#333;font-size: 1em;font-style: italic;border-radius: 50px;padding: 8px 16px;background-color:#04C5F9; color:white;">
 		<button onClick="location.href='loanapplication.php'">Apply for loan</button> </div>
   </form>
+    </div>-->
+	<div class="container">
+  <h2>Vertical (basic) form</h2>
+  <form action="/action_page.php">
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
     </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+    </div>
+    <div class="checkbox">
+      <label><input type="checkbox" name="remember"> Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+</div>
+
 </body>
 </html>
