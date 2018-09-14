78b55940919d73643f4595d3f7d0da5c0c9f74fe
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-14 16:41:50 +0530

    Update carloan.php

--- carloan.php
+++ carloan.php
@@ -2,7 +2,19 @@
 <title>Car Loan</title>
 <head>
 <style>
-
+label {
+    padding: 12px 12px 12px 0;
+    display: inline-block;
+    text-align: right;
+}
+  
+  input[type = number]{
+    width: 300px;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    resize: vertical;
+}
 </style>
 </head>
   <?php
@@ -12,8 +24,10 @@
   <form action="loanapplication.php" method="post">
     <label for="loanprincipal">Loan Amount</label>
     <input type="number" name="loanprincipal">
+    <br>
     <label for="tenure">Tenure In Months</label>
     <input type="number" name="tenure">
+    <br>
     <button onClick="location.href='loanapplication.php'">Apply for loan</button>
   </form>
 </body>
