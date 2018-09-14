cc3bac26f64ba07c5526c204a9c0565554db5fe1
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-14 15:37:25 +0530

    Update carloan.php

--- carloan.php
+++ carloan.php
@@ -5,7 +5,16 @@
 
 </style>
 </head>
+  <?php
+    $loanprincipal="";
+  ?>
 <body>
-
+  <form>
+    <label for="loanprincipal">Loan Amount</label>
+    <input type="number" name="loanprincipal">
+    <label for="tenure">Tenure In Months</label>
+    <input type="number" name="tenure">
+   
+  </form>
 </body>
 </html>
