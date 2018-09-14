fb1f1c1fff3effc883827cbf23a1b4d6df9b8126
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-14 19:11:59 +0530

    Update home.php

--- home.php
+++ home.php
@@ -195,11 +195,19 @@ li a.active {
 	</div>
 </div>
 
-<ul>
-  <li><a href="home.php">Home</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li><a href="#about">About</a></li>
-</ul>
+	<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="home.php">Home</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">About Us</a></li>
+     
+    </ul>
+  </div>
+</nav>
 <br>
 
 	
