58ff931894680e5ae1b410b8a22528762a46cd13
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-14 16:23:41 +0530

    Update carloan.php

--- carloan.php
+++ carloan.php
@@ -14,7 +14,7 @@
     <input type="number" name="loanprincipal">
     <label for="tenure">Tenure In Months</label>
     <input type="number" name="tenure">
-   
+    <button onClick="location.href='loanapplication.php'">Apply for loan</button>
   </form>
 </body>
 </html>
