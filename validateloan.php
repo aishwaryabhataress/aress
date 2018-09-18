c9f38820ef139ca951dc931b599564af08fc2389
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-18 19:51:46 +0530

    Update validateloan.php

--- validateloan.php
+++ validateloan.php
@@ -1,14 +1,11 @@
 <?php
-session_start();
-  $db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
+
+$db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
 if (!$db) 
 {
 	echo "An error occurred.\n";
 	exit;	
 }
-
- //$loanprincipal=$tenure=$annualincome="";
-
 
 echo $_GET['carloan'];
 $principal = $_POST['loanprincipal'];
