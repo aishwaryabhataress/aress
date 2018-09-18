2682cd2904fc8e43a73c3076aaf74ea7070809a3
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-18 20:28:34 +0530

    Update validateloan.php

--- validateloan.php
+++ validateloan.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 echo $_SESSION["carloan"];
 $db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
 if (!$db) 
