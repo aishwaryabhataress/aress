4a662e55f220eceab355af9e1e1c01aa6f6e2f8b
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-18 18:29:43 +0530

    Update home.php

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
  <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
 
 <style>
 
@@ -191,10 +191,7 @@ li a.active {
 	
 </style>
 	<script type="text/javascript">
-		
-		
-		
-		
+				
 		function OnClickButton() {
 		var fired_button = $("button").val();
     		alert(fired_button);
