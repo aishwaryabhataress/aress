cfafbc8bc72cd96dffb52a9c066ff379bda2357c
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-18 11:19:12 +0530

    Update home.php

--- home.php
+++ home.php
@@ -246,7 +246,11 @@ li a.active {
 	<div style="text-align:center">
   		<span class="dot" onclick="currentSlide(1)"></span> 
   		<span class="dot" onclick="currentSlide(2)"></span> 
-  		<span class="dot" onclick="currentSlide(3)"></span> 
+  		<span class="dot" onclick="currentSlide(3)"></span>
+		<span class="dot" onclick="currentSlide(4)"></span>
+		<span class="dot" onclick="currentSlide(5)"></span>
+		<span class="dot" onclick="currentSlide(6)"></span>
+		
 	</div>
 	<div class="col-sm-12 col-lg-4 col-lg-4" >
 	
