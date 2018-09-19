d1cb458ed8c8560ad320f7ba6148aec51cecfc06
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-19 19:34:46 +0530

    Update searchresults.php

--- searchresults.php
+++ searchresults.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+
 
 
 $db = pg_connect("host=ec2-54-235-212-58.compute-1.amazonaws.com port=5432 dbname=d11ltu6a8ne38d user=pkdtdgarpbsxgk password=8566866e71a89e3f3eadc11f4960e689801bfad888b96279954e1a09f94ba443");
@@ -11,6 +11,7 @@ if (!$db)
 
 $query = "SELECT FirstName ,LastName FROM salesforce.account where FirstName =$_POST['firstname'] AND LastName=$_POST['lastname'];";
 $result = pg_query($query);
+echo $result;
 if(sizeof($result)){
 echo $result;
 }
