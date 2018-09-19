3dfaab9a2a15863bd682d1bee1bd59439c051c17
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-19 10:36:01 +0530

    Update home.php

--- home.php
+++ home.php
@@ -281,9 +281,10 @@ li a.active {
 					<button class="bttn" onClick="location.href='carloan.php'">Work Capital loan</button>  
 				</div>
 			</div>
-			
+			<div style="width: 100%; position: relative; top: 50px;">
 		<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
 		<a class="next" onclick="plusSlides(1)">&#10095;</a>
+	</div>
 	<br>
 	<div style="text-align:center">
   		<span class="dot" onclick="currentSlide(1)"></span> 
