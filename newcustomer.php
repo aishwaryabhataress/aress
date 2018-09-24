15529a05c1a2a848bc9dfc49f029daf239c48d61
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-09-24 13:07:52 +0530

    Update newcustomer.php

--- newcustomer.php
+++ newcustomer.php
@@ -47,6 +47,17 @@
     </style>
     
   </head>
+	<?php 
+	$db = pg_connect("host=ec2-54-227-244-12.compute-1.amazonaws.com  port=5432 dbname=d544g2lkvj2uqn user=jwvfktfyuyftwy password=cd2fd6c6fe21c91ec21198788e2eb438b39d7b609127f5dde02af95849278ecc");
+	if (!$db) 
+	{
+		echo "An error occurred.\n";
+		exit;	
+	}
+	
+	
+	
+	?>
   <body>
     <form method="POST" >
       <div class = "container">
